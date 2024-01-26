775afc576f104099c63869ccac28bce963025275
vladisluw <komkov222111@gmail.com>
2024-01-26 20:04:49 +0500

    Добавлен модуль scrollable и деструктуризация кода

---
+++ scrollable.js
@@ -0,0 +1,34 @@
+export default (component) => {
+  let isScrolling = false
+  let startX, startY, scrollLeft, scrollTop
+
+  const container = document.querySelector(component)
+  container.style.overflow = 'scroll'
+
+  container.addEventListener('mousedown', (e) => {
+    isScrolling = true
+    startX = e.pageX - container.offsetLeft
+    startY = e.pageY - container.offsetTop
+    scrollLeft = container.scrollLeft
+    scrollTop = container.scrollTop
+  })
+
+  container.addEventListener('mouseleave', () => {
+    isScrolling = false
+  })
+
+  container.addEventListener('mouseup', () => {
+    isScrolling = false
+  })
+
+  container.addEventListener('mousemove', (e) => {
+    if (!isScrolling) return
+    e.preventDefault()
+    const x = e.pageX - container.offsetLeft
+    const y = e.pageY - container.offsetTop
+    const walkX = (x - startX)
+    const walkY = (y - startY)
+    container.scrollLeft = scrollLeft - walkX
+    container.scrollTop = scrollTop - walkY
+  })
+}
